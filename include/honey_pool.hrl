54ef3f27bd6b5e253a115a990a09e6134922f2a2
Yoko OYAMA <yowcow@gmail.com>
2025-07-18 20:23:26 +0900

    feat: Add EDoc documentation to remaining public modules

--- include/honey_pool.hrl
+++ include/honey_pool.hrl
@@ -1,3 +1,4 @@
+-doc "Represents a parsed URI.".
 -record(uri, {
           host = "" :: string(),
           path = "" :: string(),
@@ -7,6 +8,7 @@
           transport = tcp :: transport()
          }).
 
+-doc "Represents the state of a honey_pool_worker.".
 -record(state, {
           tabid :: ets:tid(),
           gun_opts = #{} :: gun_opts(),
@@ -14,13 +16,26 @@
           await_up_timeout = 5000 :: timeout()
          }).
 
+-doc "Gun options.".
 -type gun_opts() :: gun:opts().
+
+-doc "Gun request options.".
 -type gun_req_opts() :: gun:req_opts().
 
+-doc "A gun connection.".
 -type conn() :: {pid(), monitor_ref()}.
+
+-doc "A monitor reference.".
 -type monitor_ref() :: reference().
 
+-doc "The transport protocol.".
 -type transport() :: tcp | tls.
+
+-doc "Information about a host.".
 -type hostinfo() :: {Host :: string(), Port :: integer(), Transport :: transport()}.
+
+-doc "The state of a honey_pool_worker.".
 -type state() :: #state{}.
+
+-doc "A parsed URI.".
 -type uri() :: #uri{}.
